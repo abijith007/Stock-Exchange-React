978f1fd57d46ed85263b9ee933a8f4f2b70ec988
Abijith <abijithtrichurr.cs17@rvce.edu.in>
2021-07-04 13:32:59 +0530

    Inital Setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+      <Switch>
+      </Switch>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
